7907625f162e1eeac66a523ed301ed69207533d4
iame6162013 <iame6162013@gmail.com>
2017-12-24 14:35:48 +0100

    Fix typo

--- src/clLapack_source_code.cpp
+++ src/clLapack_source_code.cpp
@@ -10,7 +10,7 @@ std::string getFloatPotrfSourceCode(){
 	std::ifstream floatPotrfSourceCode("~/coriander/kernels/floatPotrfSourceCode.cl");
 	if(floatPotrfSourceCode){
 		std::string src;
-		floatPotrfSourceCode.seekg(0, td::ios::end);
+		floatPotrfSourceCode.seekg(0, std::ios::end);
 		src.resize(floatPotrfSourceCode.tellg());
 		floatPotrfSourceCode.seekg(0, std::ios::beg);
 
