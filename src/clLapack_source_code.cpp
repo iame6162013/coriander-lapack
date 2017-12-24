32d8d7dd6393f8f1461ac584e6cd73c43f25f5be
iame6162013 <iame6162013@gmail.com>
2017-12-24 17:18:12 +0100

    More logging

--- src/clLapack_source_code.cpp
+++ src/clLapack_source_code.cpp
@@ -7,14 +7,22 @@
 
 
 std::string getFloatPotrfSourceCode(){
-	std::ifstream floatPotrfSourceCode("~/coriander/kernels/floatPotrfSourceCode.cl");
+	std::cout << "a" << std::endl;
+	std::ifstream floatPotrfSourceCode("~/coriander/kernels/floatPotrfSourceCode.cl", std::ios::in);
 	if(floatPotrfSourceCode){
+		std::cout << "b" << std::endl;
 		std::string src;
 		floatPotrfSourceCode.seekg(0, std::ios::end);
+		std::cout << "x" << std::endl;
 		src.resize(floatPotrfSourceCode.tellg());
+		std::cout << "g" << std::endl;
 		floatPotrfSourceCode.seekg(0, std::ios::beg);
 
+		std::cout << "t" << std::endl;
 		floatPotrfSourceCode.read(&src[0], src.size());
+		std::cout << "y" << std::endl;
+		floatPotrfSourceCode.close();
+		std::cout << "z" << std::endl;
 		return src;
 	}
 	throw("floatPotrfSourceCode not found!");
